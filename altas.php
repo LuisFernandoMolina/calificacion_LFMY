61a5477f78b6dc780aff200049cdc7798f3eaead
RaulHernandezDiaz <31258759+RaulHernandezDiaz@users.noreply.github.com>
2017-08-24 11:44:38 -0500

    Moficacion
    Modifique el archivo altas, valta verificar valores autoincrementables y agregar boto en index

---
+++ altas.php
@@ -0,0 +1,29 @@
+<?php
+	include 'conexion.php';
+
+	$Nombre = $_POST["Nombre"];
+	$clave = $_POST["clave"];
+
+	$verifica="SELECT COUNT(Nombre)FROM usuarios WHERE Nombre='$nombre'";
+	$verifica=mysqli_query($conexion,$verifica);
+	$verifica=mysqli_query($verifica));
+	if($verifica[0]>="1")
+	{
+
+	}
+	else
+	{
+		$insertar ="INSERT INTO usuarios (nombre, clave) VALUES('$nombre, $clave')";
+		$resultado = mysqli_quert($conexion, $insertar);
+
+		if(!$resultado){
+			echo 'Error al registrarse';
+		}
+		else{
+
+		}
+		mysqli_close($conexion);
+	}
+
+?>
+
