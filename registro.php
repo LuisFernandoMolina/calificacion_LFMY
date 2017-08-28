076e003ca13068ef298ea582c151fe6a7ce4d32e
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-08-28 13:20:25 -0500

    Estilos

--- registro.php
+++ registro.php
@@ -14,17 +14,20 @@
 			<input type="text" placeholder="&#128104; Usuario" name="Nombre">
 			<input type="password" placeholder="&#128272; Constraseña" name="clave">
 			<input type="password" placeholder="&#128272; Confirmar Contraseña" name="confclave">
+			<form>
 			<div class="radio">
 				<h2>Tipo de usuario</h2>
 				<input type="radio" name="tipou" id="director" value="0">
-				<label for="director">Director</label>
+				<label for="director">Director</label><br />
 				<input type="radio" name="tipou" id="docente" value="1">
-				<label for="docente">Docente</label>
+				<label for="docente">Docente</label><br />
 				<input type="radio" name="tipou" id="estudiante" value="2">
 				<label for="estudiante">Estudiante</label>
 				<input type="radio" name="tipou" id="coordinador" value="0">
 				<label for="coordinador">Coordinador</label>
 			</div>
+			</form>
+			</form>
 				
 	</body>
 <?php>
