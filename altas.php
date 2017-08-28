4ae16fa5f60d8995d31302ddd72e297f8f355df0
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-08-28 11:56:42 -0500

    Validar claves
    Validar que coincidan y que no sean muy cortas

--- altas.php
+++ altas.php
@@ -3,10 +3,20 @@
 
 	$Nombre = $_POST["Nombre"];
 	$clave = $_POST["clave"];
-
-	$verifica="SELECT COUNT(Nombre)FROM usuarios WHERE Nombre='$nombre'";
+    // Realiza la validación de contraseñas
+	$error_message = "";
+	if (strlen($clave) < 6) {
+		$error_message = "La contraseña es demasiado corta. Por favor, introduzca al menos 6 caracteres";
+	} else if ( $clave != $_POST["confclave"]) {
+		$error_message = "Las contraseñas no coinciden. Por favor, inténtelo de nuevo";
+	}
+	echo $error_message;
+	
+	$verifica="SELECT COUNT(Nombre)FROM usuarios WHERE Nombre='$Nombre'";
 	$verifica=mysqli_query($conexion,$verifica);
-	$verifica=mysqli_query($verifica));
+	//$verifica=mysqli_query($verifica);
+	
+	
 	if($verifica[0]>="1")
 	{
 		echo 'El usuario ya existe';
@@ -15,12 +25,11 @@
 	{
 		$insertar ="INSERT INTO usuarios (nombre, clave) VALUES('$nombre, $clave')";
 		$resultado = mysqli_quert($conexion, $insertar);
-
 		if(!$resultado){
 			echo 'Error al registrarse';
 		}
 		else{
-
+			echo 'Registro exitoso';
 		}
 		mysqli_close($conexion);
 	}
---
+++ registro.php
@@ -0,0 +1,20 @@
+<html lang="es">
+
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Nuevo registro</title>
+		<meta name="viewport" content="width=divice-width,
+			user-scalable=no,initial-scale=1,maximum-scale=1,
+			minimum-scale=1">
+			<link rel="stylesheet" href="css\estilos.css">
+	</head>
+	<body>
+		<form action="altas.php" method="POST" class="altas">
+			<h2>Nuevo Registro</h2>
+			<input type="text" placeholder="&#128104; Usuario" name="Nombre">
+			<input type="password" placeholder="&#128272; Constraseña" name="clave">
+			<input type="password" placeholder="&#128272; Confirmar Contraseña" name="confclave">
+			<input type="submit" value="Registrar">		
+			<input type="submit" value="Cancelar">		
+	</body>
+</html>
