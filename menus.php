f696e7a3e3d696ceb413cb0073cb798a9a83c528
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-04 00:00:18 -0500

    cambio mal

--- menus.php
+++ menus.php
@@ -1,12 +1,14 @@
 <?php
   session_start();
   if(isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])){ //revisamos que haya iniciado sesion
-require 'conexion.php'; //aqui cargas la coneccion a tu BD
-$sql = mysql_query('SELECT * FROM usuarios WHERE usuario="'.$_SESSION['usuario'].'"'); //seleccionamos al usuario que inició sesión
-$datos = mysql_fetch_object($sql);
-  switch($datos->Tipo_usuarios_idTipos_usuarios){
-     case 'administrador':
-         //aqui muestras los tab para el administrador
+    require 'conexion.php'; //aqui cargas la coneccion a tu BD
+    $sql = mysql_query('SELECT * FROM usuarios WHERE usuario="'.$_SESSION['usuario'].'"'); //seleccionamos al usuario que inició sesión
+    echo $_SESSION['tipo'];
+    $datos = mysql_fetch_object($sql);
+    echo $sql;
+  switch($datos->Tipos_usuarios_idTipos_usuarios){
+     case '1':
+         header("location:home.php");
      break;
      case 'normal':
          //aqui muestras los tab para el usuario normal
