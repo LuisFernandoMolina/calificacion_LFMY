d2d17fe47d652d76a99b044be9c81c7cc5aa9675
RaulHernandezDiaz <31258759+RaulHernandezDiaz@users.noreply.github.com>
2017-09-04 15:30:33 -0500

    Se actulizaron archivos y carpetas
    Nada sobresaliente, solo pruebas

---
+++ ModificarUsuarios.php
@@ -0,0 +1,19 @@
+<?php>
+
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Confirma IdUsuarios</title>
+		<meta name="viewport" content="width=divice-width,
+			user-scalable=no,initial-scale=1,maximum-scale=1,
+			minimum-scale=1">
+			<link rel="stylesheet" href="css\estilos.css">
+	</head>
+	<body>
+		<form action="eliminarU.php" method="POST" >
+			<h2>Confirma el id que deseas Modificar</h2>
+			<input type="text" placeholder="&#128104; idUsuarios" name="idUsuarios" required>
+			<input type="submit" value="Modificar">
+			</form>
+
+	</body>
+<?php>
