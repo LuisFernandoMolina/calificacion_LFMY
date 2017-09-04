5bd02a9f00bbf4909d140565b712e89935f87c04
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-04 01:22:25 -0500

    que onda

--- menus.php
+++ menus.php
@@ -2,10 +2,11 @@
   session_start();
   if(isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])){ //revisamos que haya iniciado sesion
     require 'conexion.php'; //aqui cargas la coneccion a tu BD
-    $sql = mysql_query("SELECT * FROM usuarios WHERE Nombre='".$_SESSION['usuario']."'"); //seleccionamos al usuario que inició sesión
+    $session =true;
+    $sql = mysql_query("SELECT idUsuarios FROM usuarios WHERE Nombre='Fer'"); //seleccionamos al usuario que inició sesión
     echo $_SESSION['usuario'];
     echo $sql;
-    $datos = mysql_fetch_object($sql);
+    $datos = mysqli_fetch_array($sql,1);
     echo $sql;
   switch($datos->Tipos_usuarios_idTipos_usuarios){
      case '1':
@@ -15,5 +16,8 @@
          //aqui muestras los tab para el usuario normal
      break;
   }
+}
+else {
+  $session=false;
 }
 ?>
