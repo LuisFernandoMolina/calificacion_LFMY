f443a429309eeb4e0a18d463e30d4f7c98d03357
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-04 00:56:07 -0500

    a

--- menus.php
+++ menus.php
@@ -3,7 +3,7 @@
   if(isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])){ //revisamos que haya iniciado sesion
     require 'conexion.php'; //aqui cargas la coneccion a tu BD
     $sql = mysql_query("SELECT * FROM usuarios WHERE Nombre='".$_SESSION['usuario']."'"); //seleccionamos al usuario que inició sesión
-    //echo $_SESSION['usuario'];
+    echo $_SESSION['usuario'];
     echo $sql;
     $datos = mysql_fetch_object($sql);
     echo $sql;
