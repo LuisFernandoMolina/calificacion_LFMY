e8a28375d4dd79e47791d6ed98b0199e05908fca
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-08 17:36:25 -0500

    hipervinculo correcto

--- eliminarU.php
+++ eliminarU.php
@@ -4,8 +4,8 @@ $idUsuarios=$_POST['idUsuarios'];
 $query = "DELETE  FROM usuarios WHERE idUsuarios='$idUsuarios' LIMIT 1";
     if(mysqli_query($conexion, $query)) {
 	     echo '<p>Usuario Borrado Exitosamente.</p>';
-	     //sleep(10);
-	     header("location:index.html");
+	     sleep(10);
+	     header("location:DB_Usuarios.php");
      }else{
 	      echo '<p>El usuario no se pudo borrar.</p>';
     }
