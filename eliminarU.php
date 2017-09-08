4ab589d2f81562f904711529df83d2d0b301fe7f
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-08 17:37:05 -0500

    Quito el sleep

--- eliminarU.php
+++ eliminarU.php
@@ -4,7 +4,7 @@ $idUsuarios=$_POST['idUsuarios'];
 $query = "DELETE  FROM usuarios WHERE idUsuarios='$idUsuarios' LIMIT 1";
     if(mysqli_query($conexion, $query)) {
 	     echo '<p>Usuario Borrado Exitosamente.</p>';
-	     sleep(10);
+	     //sleep(10);
 	     header("location:DB_Usuarios.php");
      }else{
 	      echo '<p>El usuario no se pudo borrar.</p>';
