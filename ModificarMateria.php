010b76fd074a5f64f77bdb195d91b65f9b4e00b0
RaulHernandezDiaz <31258759+RaulHernandezDiaz@users.noreply.github.com>
2017-09-09 18:42:22 -0500

    Formualrio Materias
    Avnce, de formalri de materias, aun no esta terminado

---
+++ ModificarMateria.php
@@ -0,0 +1,75 @@
+<?php
+	require "conexion.php";
+	$idMaterias=$_POST['idMaterias'];
+	$consulta="SELECT * FROM materias WHERE idMaterias='$idMaterias'";
+	$consulta=mysqli_query($conexion, $consulta);
+	$dato=mysqli_fetch_array($consulta);
+?>
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.css" rel="stylesheet">
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<head>
+	<title> Modificar Materia </title>
+</head>
+<body>
+<h1><center>Moficacion de Materias</center></h1>
+<div class="container">
+
+	<form class="form-horizontal" action="modificarM.php" method="POST" >
+	<div class="form-group">
+		<label class="col-sm-2 control-label">Datos de la Materia </label>
+
+	</div>
+	<div class="form-group">
+
+		<label for="idMaterias" class="col-sm-2 control-label">Id Materia</label>
+		<div class="col-sm-10">
+		 <input class="form-control" style="WIDTH:100px; HEIGHT:30px" size=32 input name="idMaterias" class="form-input" value=<?php echo "\"".$dato['idMaterias']."\""; ?> required>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="Nombre" class="col-sm-2 control-label">Nombre</label>
+		<div class="col-sm-10">
+			<input  class="form-control" style="WIDTH:350px; HEIGHT: 30px" size=32 input name="Nombre_materia" class="form-input" value=<?php echo "\"".$dato['Nombre_materia']."\""; ?> required>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="clave" class="col-sm-2 control-label">Clave</label>
+		<div class="col-sm-10">
+			<input class="form-control" style="WIDTH:350px; HEIGHT: 30px" size=32 input name="clave" class="form-input" value=<?php echo "\"".$dato['clave']."\""; ?> required>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="tipo_usuario" class="col-sm-2 control-label">Tipo de usuario</label>
+		<div class="col-sm-10">
+			<select class="form-control" class="form-input" input name="Tipos_usuarios_idTipos_usuarios" id="Tipos_usuarios_idTipos_usuarios">
+					<?php include 'funciones.php'; tipou($dato['Tipos_usuarios_idTipos_usuarios']) ?>
+					<option value="0" input name="Tipos_usuarios_idTipos_usuarios">Usuario</option>
+					<option value="1"name="Tipos_usuarios_idTipos_usuarios">Director</option>
+					<option value="2"name="Tipos_usuarios_idTipos_usuarios">Maestro</option>
+					<option value="3"name="Tipos_usuarios_idTipos_usuarios">Estudiante</option>
+					<option value="4"name="Tipos_usuarios_idTipos_usuarios">Coordiandor</option>
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<button type="submit" class="btn btn-success" value="Modificar Usuario">Modificar</button>
+			<a href="DB_Usuarios.php" class="btn btn-primary">Regresar</a>
+		</div>
+
+	</div>
+
+</div>
+
+
+ <br /><h3> <!--<a ALIGN=center href="DB_Usuarios.php">REGRESAR</a></h3>-->
+<br /> <center>
+</center>
+</form>
+</body>
+</html>
