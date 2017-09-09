b9281de79b429ce5a49185ffa5d1cf20df6e65aa
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-08 23:52:27 -0500

    acomodo de menus

--- DB_Materias.php
+++ DB_Materias.php
@@ -83,12 +83,13 @@ $resultado = $conexion->query($sql);
 			</div>
 		</div>
 
-		<script>
+	<!--	<script>
 			$('#confirm-delete').on('show.bs.modal', function(e) {
 				$(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
 
 				$('.debug-url').html('Delete URL: <strong>' + $(this).find('.btn-ok').attr('href') + '</strong>');
 			});
 		</script>
+	-->
 </body>
 </html>
