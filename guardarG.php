564ef15ab2e99a64b5d4564c37ced44aa263b90d
LuisFernandoMolina <31258733+LuisFernandoMolina@users.noreply.github.com>
2017-09-09 19:07:38 -0500

    ya quedo
    falta borrar y modificar

--- guardarG.php
+++ guardarG.php
@@ -1,37 +1,32 @@
 <?php
 require 'conexion.php';
 
-$Clave_grupo= $_POST["Nombre_materia"];
-$Horario = $_POST["Horario"];
-$Salon = $_POST["Salon"];
-$Grupos_idGrupos=$_POST["idGrupo"];
-$Calificacion_idCalificacion=$_POST["calificacion"];
+$Clave_grupo= $_POST["Clave_grupo"];
+
 
 /*foreach ($_POST['Tipos_usuarios_idTipos_usuarios'] as $Tipos_usuarios_idTipos_usuarios);*/
 $error_message = "";
 //Validar usuario no este vacio
-if($Nombre_materia==""){
-  $error_message="Ingrese un nombre de Materia";
+if($Clave_grupo==""){
+  $error_message="Ingrese la clave del grupo";
 }
 echo $error_message;
 
-$verifica="SELECT * FROM materias WHERE Nombre_materia='$Nombre_materia'";
+$verifica="SELECT * FROM grupos WHERE Clave_grupo='$Clave_grupo'";
 $verifica=mysqli_query($conexion,$verifica);
 $fil=mysqli_num_rows($verifica);
   if($fil>0){
-    echo 'El usuario ya existe';
+    echo 'El grupo ya existe';
   }
   else
   {
-    $insertar ="INSERT INTO materias (Nombre_materia, Horario, Salon,Grupos_idGrupos,Calificacion_idCalificacion) VALUES('$Nombre_materia', '$Horario', '$Salon','$Grupos_idGrupos','$Calificacion_idCalificacion')"; //falta el parametro de tipo de usuario
+    $insertar ="INSERT INTO grupos (Clave_grupo) VALUES('$Clave_grupo')"; //falta el parametro de tipo de usuario
     $resultado = mysqli_query($conexion, $insertar);
     if(!$resultado){
-      echo 'Error al registar Materia';
-      echo $Grupos_idGrupos;
-      echo $Grupos_idGrupos;
+      echo 'Error al registar el Grupo';
     }
     else{
-        header("index.php");
+        header("/constitucion/calificacion_LFMY/DB_Grupos.php");
     }
     mysqli_close($conexion);
   }
@@ -55,7 +50,7 @@ $fil=mysqli_num_rows($verifica);
 						<?php } else { ?>
 						<h3>ERROR AL GUARDAR</h3>
 					<?php } ?>
-					<a href="DB_Materias.php" class="btn btn-primary">Regresar</a>
+					<a href="DB_Grupos.php" class="btn btn-primary">Regresar</a>
 				</div>
 			</div>
 		</div>
