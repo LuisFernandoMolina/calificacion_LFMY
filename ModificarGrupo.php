664d1d6cbd064c6686546f3fd2791a8fd55a46a9
RaulHernandezDiaz <31258759+RaulHernandezDiaz@users.noreply.github.com>
2017-09-09 20:20:52 -0500

    a
    a

--- ModificarGrupo.php
+++ ModificarGrupo.php
@@ -44,7 +44,7 @@
 	<div class="form-group">
 		<div class="col-sm-offset-2 col-sm-10">
 			<button type="submit" class="btn btn-success" value="Modificar Materia">Modificar</button>
-			<a href="DB_Materias.php" class="btn btn-primary">Regresar</a>
+			<a href="DB_Grupos.php" class="btn btn-primary">Regresar</a>
 		</div>
 
 	</div>
