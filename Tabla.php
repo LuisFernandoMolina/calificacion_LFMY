a1935b9937f8dda1d30bd769dd48c379d6baf669
RaulHernandezDiaz <31258759+RaulHernandezDiaz@users.noreply.github.com>
2017-10-11 16:51:03 -0500

    Saca datos de la tabla, es el de prueba 2
    Ya saca los datos

--- Tabla.php
+++ Tabla.php
@@ -5,13 +5,13 @@
 	//http://panamahitek.com/graficas-con-canvasjs/
 	//http://www.maani.us/charts4/index.php?menu=Tutorial&submenu=Chart_Data
 	//Prueba de como debe quedar la tabla
-	$chart ['chart_data'] = array ( array ("Materias",     "Quimica", "Fisica"),
+	/*$chart ['chart_data'] = array ( array ("Materias",     "Quimica", "Fisica"),
 									array ("Alumnos",       "",        ""     ),
 									array ("Fernando",         10,        9    ),
 									array ("Cecilia",           9,        8   ),
 									array ("Raul",              10,       8  )
 								);
- 	print_r($chart);
+ 	print_r($chart);*/
 
 
 
@@ -19,15 +19,46 @@
 
 	//Aqui es el codigo para nuestra tabla
 	// Con esto conectamos la base de datos
-	/*require 'conexion.php';
+	require 'conexion.php';
+	
+	//Prueba 2:
 
-	mysql_select_db('chart') or die('No se pudo selecciona la base de datos');
+	//http://panamahitek.com/graficas-con-canvasjs/
+	$data_points = array();
+	$result = mysqli_query($conexion, "SELECT * FROM usuarios");
+	while ($row = mysqli_fetch_array($result)){
+		$point = array("idUsuarios" => $row['Id'], "Nombre" => $row['Nombre'], "clave" => $row['clave'], "Tipos_usuarios_idTipos_usuarios" => $row['Tipos_usuarios_idTipos_usuarios']);
+		array_push($data_points, $point);
+	}
+	echo json_encode($data_points);	
 
+
+
+
+
+
+	
+	/*
 	//Preparamos la consutla
-	$SQLDatos ="SELECT * FROM datos";
+	$SQLDatos ="SELECT * FROM usuarios";
 
 	//Ejecutamos la consulta
-	$result = mysqli_query($SQLDatos); */
+	$result = mysqli_query($conexion,$SQLDatos); 
+
+	//Obtenemos el numero de filas
+	$numFilas = mysqli_num_rows($result);
+
+	//Cargamos el array los nombres de las metricas a visualizar
+	$datos[0] = array('Alumnos');
+
+	//Recorremos filas
+	for ($i=1; $i<($numFilas+1); $i++)
+	{
+		$datos[$i] = array(mysql_result($result, $i-1, "alumnos"), (int) mysql_result($result, $i-1, "Calificaciones"));
+	}
+	*/
+
+	
 
 
 
